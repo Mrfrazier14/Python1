85170d7239390af09e74a34ea45f8393cdf5be3e
Aaron Frazier Jr <afraz91@wgu.edu>
2023-04-30 17:35:08 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    print("I will be adding more code on here later")
+    print()
+    print("I love doing coding for this new world that I am in!")
+    print()
+    print("I added a few more lines!")
+    pass
